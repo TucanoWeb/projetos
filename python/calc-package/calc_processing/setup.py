8bccaaba999d1f79e29a062a076f6b116ffa53ba
TucanoWeb <suporte@tucanoweb.com.br>
2023-05-28 01:14:03 -0300

    First Package - calc_processing

--- python/calc-package/calc_processing/setup.py
+++ python/calc-package/calc_processing/setup.py
@@ -8,13 +8,13 @@ with open("requirements.txt") as f:
 
 setup(
     name="calc_processing",
-    version="0.0.1",
+    version="0.0.2",
     author="TucanoWeb",
     author_email="suporte@tucanoweb.com.br",
     description="Mult Table and array sum",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="#",
+    url="https://github.com/TucanoWeb/projetos/tree/master/python/calc-package",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
